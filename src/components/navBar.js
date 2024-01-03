2b4203e9f5146fc798f35fa971d9bb6294042264
Abby25CE <102004475+Abby25CE@users.noreply.github.com>
2024-01-03 12:05:14 -0600

    Update navBar.js

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -2,7 +2,7 @@ const NavBar = () => {
 return(
 <nav className="navbar bg-dark  text-light text-center mb-5">
 <div className="continer-fluid">
-    <h3 className="text-center"> Pediccion Meteorologica </h3>
+    <h3 className="text-center"> Pediccion Meteorológica </h3>
 </div>
 
 </nav>
